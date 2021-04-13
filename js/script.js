5470c78af4677200331f6a05c4a6b5ae9a19e7e7
Slava Pascaru <pascaruv@gmail.com>
2021-04-13 23:03:35 +0300

    Second commit - All Products page; Single Product page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    
+
     $('.menu-wrap .toggler').click(function () {
-        $('.logo').toggleClass('white black');       
+        $('.logo_text a').toggleClass('white black');       
         $('.mymenu').toggleClass('show closed');        
         $('.mymenu_list').toggleClass('dsp-bl');
         $('li').toggleClass('menu-open');        
@@ -24,3 +24,13 @@ function moveCircle(e) {
 }
 
 $(window).on('mousemove', moveCircle);
+
+window.addEventListener( "pageshow", function ( event ) {
+  var historyTraversal = event.persisted || 
+                         ( typeof window.performance != "undefined" && 
+                              window.performance.navigation.type === 2 );
+  if ( historyTraversal ) {
+    // Handle page restore.
+    window.location.reload();
+  }
+});
